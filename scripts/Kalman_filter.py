6c064e45cf3e8c8b1bc866baabdebe37fda1007a
HAR <64668639+HAR0070@users.noreply.github.com>
2023-09-26 14:13:38 +0530

    Kalman_filter.py
    updated

--- scripts/Kalman_filter.py
+++ scripts/Kalman_filter.py
@@ -1,20 +1,23 @@
 
 import numpy as np
 
+dt = 0.1
 
 x = np.array([[0],  # Initial position
               [0]]) # Initial velocity
 P = np.array([[1, 0],  # Initial position covariance
               [0, 1]]) # Initial velocity covariance
 
-A = np.array([[1, 1],  # State transition matrix
+A = np.array([[1, dt],  # State transition matrix
               [0, 1]])
 
-B = np.array([[1],  # Control matrix
-              [0]])
+B = np.array([[0],  # Control matrix
+              [dt]])
 u = np.array([[0]])  # Control input (change in position)
 
-H = np.array([[1, 0]])  # Measurement matrix
+H_odo = np.array([[1 , 1]])  # Measurement matrix for odometry
+H_lidar = np.array([[1 , 0]]) # Measurement matrix for lidar
+
 R_odometry = np.array([[0.1]])  # Odometry measurement noise covariance
 R_lidar = np.array([[0.01]])    # Lidar measurement noise covariance
 
@@ -42,14 +45,14 @@ for _ in range(50):
     P_hat = np.dot(np.dot(A, P), A.T) + Q
 
     # Kalman Gain for odometry
-    K_odometry = np.dot(np.dot(P_hat, np.array([[1], [0]]).T), np.linalg.inv(np.dot(np.dot(np.array([[1], [0]]), P_hat), np.array([[1], [0]]).T) + R_odometry))
+    K_odometry = np.dot(np.dot(P_hat, H_odo.T), np.linalg.inv(np.dot(np.dot(H_odo, P_hat), H_odo.T) + R_odometry))
 
     # Kalman Gain for lidar
-    K_lidar = np.dot(np.dot(P_hat, np.array([[0], [1]]).T), np.linalg.inv(np.dot(np.dot(np.array([[0], [1]]), P_hat), np.array([[0], [1]]).T) + R_lidar))
+    K_lidar = np.dot(np.dot(P_hat, H_lidar.T), np.linalg.inv(np.dot(np.dot(H_lidar, P_hat), H_lidar.T) + R_lidar))
 
     # Update using both measurements
-    x = x_hat + np.dot(K_odometry, (z_odometry - np.dot(np.array([[1, 0]]), x_hat))) + np.dot(K_lidar, (z_lidar - np.dot(np.array([[0, 1]]), x_hat)))
-    P = P_hat - np.dot(np.dot(np.array([[1, 0]]).T, K_odometry), P_hat) - np.dot(np.dot(np.array([[0, 1]]).T, K_lidar), P_hat)
+    x = x_hat + np.dot(K_odometry, (z_odometry - np.dot(H_odo, x_hat))) + np.dot(K_lidar, (z_lidar - np.dot(H_lidar, x_hat)))
+    P = P_hat - np.dot(np.dot(H_odo.T, K_odometry), P_hat) - np.dot(np.dot(H_lidar.T, K_lidar), P_hat)
 
     # Simulate new measurements (replace this with actual sensor readings)
     z_odometry = np.array([[2 + np.random.normal(0, 0.1)]])
