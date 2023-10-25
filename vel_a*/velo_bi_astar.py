b56b019163a43ce900a05192150c09d5d6502e16
HAR <64668639+HAR0070@users.noreply.github.com>
2023-10-26 01:49:29 +0530

    working but....

--- vel_a*/velo_bi_astar.py
+++ vel_a*/velo_bi_astar.py
@@ -10,6 +10,12 @@ import time
 
 plt.ion()
 plt.axis('equal')
+"""
+Needs work on
+    - Resolution for removing same nodes
+    - Resolution for is_goal
+    - Cost function
+"""
 
 """
 states are maintaned as dictionory
@@ -61,7 +67,7 @@ class States:
         value = self.data.pop(key)
         return key, value
 
-def propogation(u,state_p, plot = False):
+def forward_propogation(u,state_p, plot = False):
     # Initialization
     step_t = 1
     dt = 0.1
@@ -77,9 +83,14 @@ def propogation(u,state_p, plot = False):
     for i in range(N):
         # Compute the jerk (third derivative of x) using the equation
         # Update the position, velocity, and acceleration using Euler's method
-        x[i+1] = x[i] + x_dot*dt + x_doubledot*dt
+        x[i+1] = x[i] + x_dot*dt + 0.5*x_doubledot*dt*dt
         x_dot += x_doubledot * dt
-        x_doubledot = x_doubledot*0.90 + (k * u)*dt/T
+        if x_doubledot <= 2 or x_doubledot >= -3.5:
+            x_doubledot = x_doubledot*0.98  + k*u*(1 - alpha)
+        elif x_doubledot > 2:
+            x_doubledot  = 2
+        elif x_doubledot < -3.5:
+            x_doubledot = -3.5
         # Store or use the generated state x
         t[i+1] = t[i] + dt
     if plot:
@@ -104,16 +115,21 @@ def reverse_propogation(u,state_p, plot = False):
     for i in range(N):
         # Compute the jerk (third derivative of x) using the equation
         # Update the position, velocity, and acceleration using Euler's method
-        x[i+1] = x[i] - x_dot*dt - x_doubledot*dt
+        x[i+1] = x[i] - x_dot*dt - 0.5*x_doubledot*dt*dt
         x_dot -= x_doubledot * dt
-        x_doubledot = (x_doubledot - (k * u)*dt/T )/0.9
+        if x_doubledot <= 2 or x_doubledot >= -3.5:
+            x_doubledot = (x_doubledot - k*u*(1 - alpha))/0.98
+        elif x_doubledot > 2:
+            x_doubledot  = 2
+        elif x_doubledot < -3.5:
+            x_doubledot = -3.5
         # Store or use the generated state x
         t[i+1] = t[i] + dt
     if plot:
         plt.scatter(x,t)
         plt.show()
         plt.pause(0.001)
-    return x , x[-1]-x[0], x_doubledot, x_dot
+    return x , x[0]-x[-1], x_doubledot, x_dot
 
 def is_goal(neighbor, goal, do, T_hw, res):
     # goal will be x coordinate with a and v
@@ -121,130 +137,188 @@ def is_goal(neighbor, goal, do, T_hw, res):
     diff2 = (goal[1] - neighbor[1])**2
     diff3 = (goal[2] - neighbor[2])**2
     res = 0.2
-    ########
-    #
-    # correct the resolution later
-    #
-    #######
     if (diff3 < res) and (diff2 < res) and (diff1 < res):
         return True
+
+def solution_path(gkey,skey,cg_fringe,cs_fringe):
+    print("Path found by intersection")
+    path = []
+    for id in reversed(cs_fringe.parent_id(skey)):
+        path.append([cs_fringe.x(id), cs_fringe.v(id), cs_fringe.a(id), cs_fringe.input(id)])
+    for id in cg_fringe.parent_id(gkey):
+        path.append([cg_fringe.x(id), cg_fringe.v(id), cg_fringe.a(id), cg_fringe.input(id)])
+    return path
 
 def astar_v(start, goal, do, T_hw, resolution):
     # print("now running a star")
     parent = []
     id = 0
-    s_fringe = States()
+    s_fringe = States()   # open fringe
     g_fringe = States()
-    node = [*start, parent, 0 , 0]
-    s_fringe.add(id, node)
-    g_fringe.add(id,node)
-    sc_fringe = States()
-    gs_fringe = States()
+    nods = [*start, parent, 0 , 0]
+    s_fringe.add(id, nods)
+    nodg = [*goal, parent, 0,0]
+    g_fringe.add(id,nodg)
+    cs_fringe = States() # closed fringe
+    cg_fringe = States()
 
-    path = []
+    path = []  # the return path
     y = 0
     if is_goal(start, goal, do, T_hw, resolution):
-        return path
+        return 'brake'
 
-    b = 0
     jerk = [i * 0.3 for i in range(-5, 5)]
-    while s_fringe.len() >0 or g_fringe.len():
-        # print("len of the fringe is" + str(Ofringe.len()))
-        # print(f"this is the open fringe {Ofringe}")
-        b +=1
-        if s_fringe.len() >0:           """ eddited till here for biderection"""
-        key, node = Ofringe.remove()
-        # print("a node is popped", node)
-        # print(f"cost of poped node is {node[4]}")
-        parent_id = key
-        Cfringe.add(key, node)
+    res = resolution**2
 
-        for u in jerk:
-            state_p = node # copy of the parent node
+    while s_fringe.len() >0 or g_fringe.len() >0:
 
-            if abs(state_p[2] - u) > 1.5:
-                continue
+        if s_fringe.len() >0:
+            # print("opening s fringe")
+            key, nods = s_fringe.remove()
+            parent_id = key
+            cs_fringe.add(key, nods)
 
-            trajectory, ds , acel , v = reverse_propogation(u, state_p) # add false
-            neighbor = [trajectory[-1][0] , v, acel]
-            # print(f"is the x correct {trajectory[-1][0]} and {trajectory}")
-            # print("this is the neighbor", neighbor)
-
-            if acel > 2 or acel < -3.5:
-                continue
-
-            if ds < 0 or v<0:
-                continue
-
-            if neighbor[0] > goal[0]:
-                continue
-
-            if is_goal(neighbor, goal, do, T_hw, resolution):
+            if is_goal(nods[0:3], goal, do, T_hw, resolution):
                 # print("checking is_goal")
                 for id in range(0,id):
                     try:
-                        plt.scatter(Ofringe.x(id), Ofringe.v(id), marker='x')
+                        plt.scatter(s_fringe.x(id), s_fringe.v(id), marker='x')
                     except:
                         pass
-                path.append(neighbor[0:3])
-                # plt.scatter(i,i)
-                for id in reversed(state_p[3]):
-                    path.append([Cfringe.x(id), Cfringe.v(id), Cfringe.a(id)])
-                    # plt.scatter(Cfringe.input(id), Cfringe.input(id),marker='#')
+                for id in reversed(cs_fringe.parent_id(parent_id)):
+                    path.append([cs_fringe.x(id), cs_fringe.v(id), cs_fringe.a(id), cs_fringe.input(id)])
                 return path
 
-
-            ## cost
-            # h1 = (goal[0] + T_hw*v + do) - start[0]
-            # h2 = 0.1*(start[1] - goal[1])**2 +  (start[2] - goal[2])**2
-            h1 = 0
-            h2 = 0
-            # cost = (0.1/(ds + 0.1)) + 2*jerk**2 + 5*a**2
-            cost1 = (goal[0]- neighbor[0])**2 # + do
-            cost2 = (acel)**2  #+ 1/(ds + 0.1) + 2*jerk**2
-            cost3 = (goal[1] - neighbor[1])**2
-            # h2 = -(neighbor[1] - goal[1]) +  -(neighbor[2] - goal[2])
-            # print(f"huristics are {cost1} and {cost2}")
-
-            # print(f"the costs are jerk {2*jerk**2} , {2*(a-state_p[2])**2} , if ds was there {0.1/(ds + 0.1)}")
-            # print(f"this is h1 {h1} and this is h2 {h2} the cost is {cost} ")
-            cost = np.round((cost2*2+cost3*1.2+ cost1*1.1), 3)
-
-            tag = True
-            res = resolution**2
-            for key in Ofringe.key():
-                diff1 = (neighbor[0] - Ofringe.x(key))**2
-                diff2 = (neighbor[1] - Ofringe.v(key))**2
-                diff3 = (neighbor[2] - Ofringe.a(key))**2
+            for key in cg_fringe.key():
+                diff1 = (nods[0] - cg_fringe.x(key))**2
+                diff2 = (nods[1] - cg_fringe.v(key))**2
+                diff3 = (nods[2] - cg_fringe.a(key))**2
                 if (diff3 < res) and (diff2 < res) and (diff1 < res):
-                    # print("node comes inside resolution")
-                    tag = False
-                    if state_p[4] < Ofringe.cost(key):
-                        id+=1
-                        nodea = [*neighbor, [*node[3], parent_id], cost+ node[4], u]
-                        Ofringe.add(id,nodea)
-                        Ofringe.rmv_id(key)
-                        # plt.scatter(i,i)
-                        # print("appending through check", nodea)
-                        # plt.scatter(nodea[0],nodea[1], marker='+', s =20)
-                        # plt.show()
-                        # plt.pause(0.001)
-                        break
-                    else:
-                        break
+                    print("path found s intersecting g")
+                    return solution_path(key,parent_id,cg_fringe,cs_fringe)
                 else:
                     continue
-            if tag:
-                id+=1
-                nodeb = [*neighbor, [*node[3], parent_id], cost+ node[4], u]
-                Ofringe.add(id,nodeb)
-                # plt.scatter(i,i)
-                # y += 1
-                # plt.scatter(nodeb[0],nodeb[1],marker='o', s= 10)
-                # plt.show()
-                # plt.pause(0.001)
-                # print("appending directly", nodeb)
 
+            for u in jerk:
+                # print(" making sub nods")
+                state_p = nods # copy of the parent node
+
+                if abs(state_p[5] - u) > 1:
+                    continue
+
+                trajectory, ds , acel , v = forward_propogation(u, state_p) # add false
+                neighbor = [trajectory[-1][0] , v, acel]
+
+                if acel > 2 or acel < -3.5:
+                    continue
+
+                if ds < 0 or v<0:
+                    continue
+
+                if neighbor[0] > goal[0]:
+                    continue
+
+                cost = 0.2*u**2 + (cs_fringe.v(parent_id) - neighbor[1])**2 + (cs_fringe.a(parent_id) - acel)**2 + (cs_fringe.v(parent_id)*1.5 - ds)**2
+                h2 = 3*(goal[0] - neighbor[0])**2  + acel**2
+                cost = h2 + cost
+
+                tag = True
+                for key in s_fringe.key():
+                    diff1 = (neighbor[0] - s_fringe.x(key))**2
+                    diff2 = (neighbor[1] - s_fringe.v(key))**2
+                    diff3 = (neighbor[2] - s_fringe.a(key))**2
+                    if (diff3 < res) and (diff2 < res) and (diff1 < res):
+                        # print("node comes inside resolution")
+                        tag = False
+                        if state_p[4] < s_fringe.cost(key):
+                            id+=1
+                            nodea = [*neighbor, [*nods[3], parent_id], cost+ nods[4], u]
+                            s_fringe.add(id,nodea)
+                            s_fringe.rmv_id(key)
+                            break
+                        else:
+                            break
+                    else:
+                        continue
+                if tag:
+                    id+=1
+                    nodeb = [*neighbor, [*nods[3], parent_id], cost+ nods[4], u]
+                    s_fringe.add(id,nodeb)
+
+        if g_fringe.len() >0:
+            # print("Opening g fringe")
+            key, nodg = g_fringe.remove()
+            parent_id = key
+            cg_fringe.add(key, nodg)
+
+            if is_goal(nodg[0:3], start, do, T_hw, resolution):
+                # print("checking is_goal")
+                for id in range(0,id):
+                    try:
+                        plt.scatter(g_fringe.x(id), g_fringe.v(id), marker='x')
+                    except:
+                        pass
+                for id in cg_fringe.parent_id(parent_id):
+                    path.append([cg_fringe.x(id), cg_fringe.v(id), cg_fringe.a(id), cg_fringe.input(id)])
+                return path
+
+            for key in cs_fringe.key():
+                diff1 = (nodg[0] - cs_fringe.x(key))**2
+                diff2 = (nodg[1] - cs_fringe.v(key))**2
+                diff3 = (nodg[2] - cs_fringe.a(key))**2
+                if (diff3 < res) and (diff2 < res) and (diff1 < res):
+                    print("path found g intersecting s")
+                    return solution_path(parent_id,key,cg_fringe,cs_fringe)
+                else:
+                    continue
+
+            for u in jerk:
+                state_p = nodg # copy of the parent node
+
+                if abs(state_p[5] - u) > 1.5:
+                    continue
+
+                trajectory, ds , acel , v = reverse_propogation(u, state_p) # add false
+                neighbor = [trajectory[-1][0] , v, acel]
+
+                if acel > 2 or acel < -3.5:
+                    # print("nodg droppping here for acel")
+                    continue
+
+                if ds < 0 or v<0:
+                    # print("nodg dropping here for ds and v")
+                    continue
+
+                if neighbor[0] < start[0]:
+                    continue
+
+                # print("producing nodg")
+                cost = 0.2*u**2 + (cg_fringe.v(parent_id) - neighbor[1])**2 + (cg_fringe.a(parent_id) - acel)**2 + (cg_fringe.v(parent_id)*1.5 - ds)**2
+                h2 = 3*(start[0] - neighbor[0])**2  + acel**2
+                cost = h2 + cost
+
+                tag = True
+                for key in g_fringe.key():
+                    diff1 = (neighbor[0] - g_fringe.x(key))**2
+                    diff2 = (neighbor[1] - g_fringe.v(key))**2
+                    diff3 = (neighbor[2] - g_fringe.a(key))**2
+                    if (diff3 < res) and (diff2 < res) and (diff1 < res):
+                        # print("node comes inside resolution")
+                        tag = False
+                        if state_p[4] < g_fringe.cost(key):
+                            id+=1
+                            nodea = [*neighbor, [*nodg[3], parent_id], cost+ nodg[4], u]
+                            g_fringe.add(id,nodea)
+                            g_fringe.rmv_id(key)
+                            break
+                        else:
+                            break
+                    else:
+                        continue
+                if tag:
+                    id+=1
+                    nodeb = [*neighbor, [*nodg[3], parent_id], cost+ nodg[4], u]
+                    g_fringe.add(id,nodeb)
 
     return 'brake'
 
@@ -252,17 +326,19 @@ def astar_v(start, goal, do, T_hw, resolution):
 if __name__=='__main__':
     # start = x , v, a
     # goal = x, v, a
+    prev_time = time.time()
     do = 0
-    T_hw = 1.5
+    T_hw = 1
     resolution = np.sqrt(0.01)
-    intial = 0 , 0 , 0
-    goal = 2 , 0 , 0
-    # fig =  plt.figure()
-    # fig.suptitle('fringe items')
-    # plt.xlabel('position')
-    # plt.ylabel('velocity')
-    # plt.title('Scatter Plot')
+    intial = 45 , 0 , 0
+    goal = 65 , 0 , 0
+    dt = 0.1
+    T = 0.532
+    alpha = math.exp(-dt / T)
+
     path = astar_v(intial, goal,do, T_hw, resolution)
+    curr_time = time.time()
+    print(f"this is the time taken {curr_time-prev_time}")
     if path != 'brake':
         print("!!!!!!!!!!!!!!!!!!!!!!###########################!!!!!!!!!!!!!!!!!!!!!!!!!!")
         path = np.array(path)
@@ -272,7 +348,7 @@ if __name__=='__main__':
         plt.plot(x,v, label = "position" ,linewidth=2.5)
         plt.scatter(goal[0], goal[1] , marker='D')
         # plt.plot(v,label = "velocity" , linewidth=2.5 )
-        plt.plot(x,a ,label = "accel",  linewidth=2.5)
+        # plt.plot(x,a ,label = "accel",  linewidth=2.5)
 
         plt.legend()
         plt.show()
